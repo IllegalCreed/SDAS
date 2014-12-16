ac5aa5334c7cd648ea1ccec4c6dd50001b501e11
IllegalCreed <illegals0001@gmail.com>
2014-12-16 21:31:37 +0800

    提交字体文件，资源字典，增加登录控件

--- SDAS/SDAS/App.xaml.cs
+++ SDAS/SDAS/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SDAS.Views;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,12 @@ namespace SDAS
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow MW = new MainWindow();
+            MW.Show();
+        }
     }
 }
--- SDAS/SDAS/Views/MainWindow.xaml.cs
+++ SDAS/SDAS/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SDAS
+namespace SDAS.Views
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑

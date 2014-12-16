1b404ca3d86ca7bd922602a7d4234bb4cdbcb1c1
Hirasawa_Ui <justieray@hotmail.com>
2014-12-16 22:30:45 +0800

    modify logic

--- SDAS/SDAS/Views/WaterTextBoxControl.cs
+++ SDAS/SDAS/Views/WaterTextBoxControl.cs
@@ -3,11 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace SDAS.Views
 {
     public class WaterTextBoxControl : TextBox
     {
+
+
+        private bool isDefaultText = true;
+        public string DefaultText
+        {
+            get { return (string)GetValue(DefaultTextProperty); }
+            set { SetValue(DefaultTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DefaultText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DefaultTextProperty =
+            DependencyProperty.Register("DefaultText", typeof(string), typeof(WaterTextBoxControl), new PropertyMetadata(""));
+
+
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            base.OnGotFocus(e);
+            if (isDefaultText)
+            {
+                this.Text = "";
+                this.isDefaultText = false;
+            }
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            this.Text = DefaultText;
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            base.OnLostFocus(e);
+            if (this.Text.Trim() == "")
+            {
+                this.Text = DefaultText;
+                this.isDefaultText = true;
+            }
+         
+        }
     }
 }

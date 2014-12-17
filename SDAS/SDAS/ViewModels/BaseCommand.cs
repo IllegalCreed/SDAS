b253ca597a1f0aac53967c243d0e66fd972a83f1
IllegalCreed <illegals0001@gmail.com>
2014-12-17 18:27:56 +0800

    增加MVVM基础类

---
+++ SDAS/SDAS/ViewModels/BaseCommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SDAS.ViewModels
+{
+    public class BaseCommand : ICommand
+    {
+        #region Members
+        private Action mCommand;
+        private Action<Object> mParaCommand;
+        private Func<Object, Boolean> mCanExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        #endregion
+
+        #region Ctor
+        public BaseCommand(Action command, Func<Object, Boolean> canExecute = null)
+        {
+            mCommand = command;
+            mCanExecute = canExecute;
+        }
+
+        public BaseCommand(Action<Object> paraCommand, Func<Object, Boolean> canExecute = null)
+        {
+            mParaCommand = paraCommand;
+            mCanExecute = canExecute;
+        }
+        #endregion
+
+        public bool CanExecute(object parameter)
+        {
+            if (null != mCanExecute)
+            {
+                return mCanExecute(parameter);
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (null != mCommand)
+            {
+                mCommand();
+            }
+
+            if (null != mParaCommand)
+            {
+                mParaCommand(parameter);
+            }
+        }
+    }
+}
---
+++ SDAS/SDAS/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDAS.ViewModels
+{
+    public class MainViewModel
+    {
+    }
+}

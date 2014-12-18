7935d58f70edee55b6ee1c1f339329f4463e50db
IllegalCreed <illegals0001@gmail.com>
2014-12-18 15:36:45 +0800

    增加销售首页，增加导航逻辑

---
+++ SDAS/SDAS/ViewModels/LoginViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SDAS.ViewModels
+{
+    public class LoginViewModel
+    {
+        private MainViewModel ParentVM;
+
+        public LoginViewModel(MainViewModel VM)
+        {
+            ParentVM = VM;
+        }
+
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new BaseCommand(OnLogin);
+            }
+        }
+
+        public void OnLogin()
+        {
+            ParentVM.IsLoginPage = false;
+            ParentVM.Pagesource = "SellerHomePage.xaml";
+        }
+    }
+}
--- SDAS/SDAS/ViewModels/MainViewModel.cs
+++ SDAS/SDAS/ViewModels/MainViewModel.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace SDAS.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        public MainViewModel()
+        {
+            LVM = new LoginViewModel(this);
+        }
+
+
+        private bool mIsLoginPage = true;
+        /// <summary>
+        /// 是否为登录页
+        /// </summary>
+        public bool IsLoginPage
+        {
+            get
+            {
+                return mIsLoginPage;
+            }
+            set
+            {
+                if (value != mIsLoginPage)
+                {
+                    mIsLoginPage = value;
+                    RaisePropertyChanged(() => IsLoginPage);
+                }
+            }
+        }
+
+        private string mPageSource;
+        public string Pagesource
+        {
+            get
+            {
+                return mPageSource;
+            }
+            set
+            {
+                if (value != mPageSource)
+                {
+                    mPageSource = value;
+                    RaisePropertyChanged(() => Pagesource);
+                }
+            }
+        }
+
+        public LoginViewModel LVM
+        {
+            get;
+            set;
+        }
     }
 }

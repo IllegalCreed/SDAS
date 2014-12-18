ebe9c9830b8c6f0fef3ac8346680a885ed261e8a
IllegalCreed <illegals0001@gmail.com>
2014-12-18 19:46:41 +0800

    update

---
+++ SDAS/SDAS/ViewModels/Converters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SDAS.ViewModels
+{
+    /// <summary>
+    /// 逆布尔值转显隐
+    /// </summary>
+    public class AntiBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Visibility Result = Visibility.Visible;
+
+            if ((bool)value)
+            {
+                Result = Visibility.Collapsed;
+            }
+            else
+            {
+                Result = Visibility.Visible;
+            }
+
+            return Result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
